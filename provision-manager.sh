c8113fd935463983547ffb7a95cc9d304c2b75c5
Jakub Woźniak <j.wozniak@tenesys.pl>
2017-01-15 15:33:19 +0100

    changed user from ubuntu to vagrant

--- provision-manager.sh
+++ provision-manager.sh
@@ -4,8 +4,8 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get install -y python-pip libssl-dev build-essential libffi-dev libpython-dev
 pip install --upgrade pip
 pip install ansible
-cp /vagrant/ansible.cfg /home/ubuntu/ansible.cfg
-chown ubuntu:ubuntu /home/ubuntu/ansible.cfg
-su ubuntu -c "ssh-keygen -t rsa -N \"\" -f /home/ubuntu/.ssh/id_rsa"
-cp /home/ubuntu/.ssh/id_rsa.pub /vagrant/
+cp /vagrant/ansible.cfg /home/vagrant/ansible.cfg
+chown vagrant:vagrant /home/vagrant/ansible.cfg
+su vagrant -c "ssh-keygen -t rsa -N \"\" -f /home/vagrant/.ssh/id_rsa"
+cp /home/vagrant/.ssh/id_rsa.pub /vagrant/
 cat /vagrant/hosts >> /etc/hosts
--- provision.sh
+++ provision.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 apt-get install -y python python-apt
-cat /vagrant/id_rsa.pub >> /home/ubuntu/.ssh/authorized_keys
+cat /vagrant/id_rsa.pub >> /home/vagrant/.ssh/authorized_keys
